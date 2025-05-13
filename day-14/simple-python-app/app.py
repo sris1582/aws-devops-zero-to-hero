ccaef225c69be933060f4ffde199bae0719445f5
sris1582 <148256801+sris1582@users.noreply.github.com>
2025-05-13 22:24:15 +0530

    Update app.py

--- day-14/simple-python-app/app.py
+++ day-14/simple-python-app/app.py
@@ -4,8 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, world!'
+    return 'Hello, world! Thank you'
 
 if __name__ == '__main__':
-    app.run() 
+    app.run()
 
